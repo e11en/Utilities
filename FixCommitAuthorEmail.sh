1a704f0f59c5651bf982ae0691d2bcd00756e978
Ellen Langelaar <ellen@rockt.nl>
2017-11-08 16:07:37 +0100

    Create FixCommitAuthorEmail.sh

---
+++ FixCommitAuthorEmail.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="SOME_EMAIL_ADDRESS"
+CORRECT_NAME="e11en"
+CORRECT_EMAIL="ellen@rockt.nl"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
